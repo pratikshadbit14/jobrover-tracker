462037e4afdbf09e611ef2c1b2e755e7f723b6ec
Pratiksha <pratikshadbit@gmail.com>
2026-06-20 13:17:36 +0530

    modified maain.py

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-))
+)
 
 app.include_router(auth.router)
 app.include_router(applications.router)
